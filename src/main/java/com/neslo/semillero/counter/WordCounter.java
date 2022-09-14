0b65cc6865faf6781348568190be631e828b5a3d
Nestor <nlopezm@devco.com.co>
2022-09-14 10:05:32 -0500

    Se crea la interfaz para un wordcounter y las clases relacionadas (summary.java)

---
+++ src/main/java/com/neslo/semillero/counter/WordCounter.java
@@ -0,0 +1,9 @@
+package com.neslo.semillero.counter;
+
+import com.neslo.semillero.model.Summary;
+
+import java.io.IOException;
+
+public interface WordCounter {
+    Summary procecc(String path) throws IOException;
+}
