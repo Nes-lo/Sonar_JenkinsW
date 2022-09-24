0944f4107c183206484416abcc7b453f41cfae39
Nestor <nlopezm@devco.com.co>
2022-09-24 15:57:24 -0500

    d

--- src/test/java/com/neslo/semillero/counter/WordCounterDefaultTest.java
+++ src/test/java/com/neslo/semillero/counter/WordCounterDefaultTest.java
@@ -2,6 +2,7 @@ package com.neslo.semillero.counter;
 
 import com.neslo.semillero.model.Summary;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -10,18 +11,41 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class WordCounterDefaultTest {
-    @Test
-    void mustReturnCorrectSummary() throws IOException {
+    private WordCounter wordCounter;
+    private String absolutePath;
+    @BeforeEach
+    //Preparacion de la prueba
+    public void setup(){
         Path resourceDirectory= Paths.get("src","test","resources");
-        String absolutePath=resourceDirectory.toFile().getAbsolutePath();
-        WordCounter wordCounter=new WordCounterDefault();
+                absolutePath=resourceDirectory.toFile().getAbsolutePath();
+        wordCounter=new WordCounterDefault();
 
+    }
+    @Test
+
+    void mustReturnCorrectSummary() throws IOException {
+
+      //Ejecuccion de la prueba
        Summary summary=wordCounter.process(absolutePath+ File.separator + "test.txt");
-
+      //Verificaicon de la prueba
        Assertions.assertEquals(2,summary.getNumberOfLines());
        Assertions.assertEquals(5,summary.getNumberOfWord());
 
+    }
+
+    void mustThrowsIOExceptionWhenFileNotFound() throws IOException{
+        IOException thrown = assertThrows(
+                IOException.class,
+                ()->{
+                    Summary summary=wordCounter.process(absolutePath+File.separator+"test.txt");
+                }
+        );
+
+        assertNotNull(thrown.getMessage());
     }
 
 }
